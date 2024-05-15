80a0b70d3e4dc47a2ef85192346005cd1a25423f
Selomon1 <soya.vandy1950@gmail.com>
2024-05-15 16:28:52 +0300

    File Manager

---
+++ utils/db.js
@@ -0,0 +1,36 @@
+import { MongoClient } from 'mongodb';
+
+
+class DBClient {
+    constructor() {
+        const host = process.env.DB_HOST || 'localhost';
+        const port = process.env.DB_PORT || 27017;
+        const database = process.env.DB_DATABASE || 'files_manager';
+        const url = `mongodb://${host}:${port}/${database}`;
+
+        this.client = new MongoClient(url, { useUnifiedTopology: true });
+
+        this.client.connect((err) => {
+            if (err) {
+                this.db = false;
+            } else {
+                this.db = this.client.db(database);
+            }
+        });
+     }
+
+    isAlive() {
+        return !!this.db;
+    }
+
+    async nbUsers() {
+        return this.isAlive() ? await this.db.collection('users').countDocuments() : 0;
+    }
+
+    async nbFiles() {
+        return this.isAlive() ? await this.db.collection('files').countDocuments() : 0;
+    }
+}
+
+const dbClient = new DBClient();
+export default dbClient;
