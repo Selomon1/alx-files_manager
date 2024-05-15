ad77a3563f72549671bed5c8ece9ac6119742391
Selomon1 <soya.vandy1950@gmail.com>
2024-05-15 18:17:00 +0300

    File Manager

--- utils/redis.js
+++ utils/redis.js
@@ -3,49 +3,49 @@ import { promisify } from 'util';
 
 
 class RedisClient {
-    constructor() {
-        this.client = createClient();
-        this.isConnected = true;
+	constructor() {
+		this.client = createClient();
+		this.isConnected = true;
 
-        this.client.on('error', (error) => {
-            console.error(error);
-            this.isConnected = false;
-        });
-    }
+		this.client.on('error', (error) => {
+			console.error(error);
+				this.isConnected = false;
+		});
+	}
 
-    isAlive () {
-        if (this.isConnected) {
-            return true;
-        }
-        return false;
-    }
+	isAlive () {
+		if (this.isConnected) {
+			return true;
+		}
+		return false;
+	}
 
-    async get(key) {
-        try {
-            const getAsync = promisify(this.client.get).bind(this.client);
-            return getAsync(key);
-        } catch (error) {
-            console.error(error);
-            return null;
-        }
-    }
+	async get(key) {
+		try {
+			const getAsync = promisify(this.client.get).bind(this.client);
+			return getAsync(key);
+		} catch (error) {
+			console.error(error);
+			return null;
+		}
+	}
 
-    async set(key, value, duration) {
-        try {
-            this.client.setex(key, duration, value);
-        } catch (error) {
-            console.error(error);
-            return null;
-        }
-    }
+	async set(key, value, duration) {
+		try {
+			this.client.setex(key, duration, value);
+		} catch (error) {
+			console.error(error);
+			return null;
+		}
+	}
 
-    async del(key) {
-        try {
-            this.client.del(key);
-        } catch (error) {
-            console.error(error);
-        }
-    }
+	async del(key) {
+		try {
+			this.client.del(key);
+		} catch (error) {
+			console.error(error);
+		}
+	}
 }
 
 const redisClient = new RedisClient();
