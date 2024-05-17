937fad9880cdc68401f285042d8543bed6f51b54
Selomon1 <soya.vandy1950@gmail.com>
2024-05-17 19:58:42 +0300

    File Manager

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,28 +1,28 @@
 import dbClient from '../utils/db';
 import redisClient from '../utils/redis';
-import sha1 from 'sha1';
+import { createHash } from 'crypto';
 import { ObjectID } from 'mongodb';
 
 
 class UsersController {
   static async postNew(req, res) {
-    const { email, password } = req.body;
-
-    if (!email) {
-      return res.status(400).json({ error: 'Missing email' });
-    }
-
-    if (!password) {
-      return res.status(400).json({ error: 'Missing password' });
-    }
-
     try {
-      const exisingUser = await dbClient.db.collection('users').findOne({ email });
-      if (existingUser) {
+      const { email, password } = req.body;
+
+      if (!email) {
+        return res.status(400).json({ error: 'Missing email' });
+      }
+
+      if (!password) {
+        return res.status(400).json({ error: 'Missing password' });
+      }
+
+      const exisingUser = dbClient.db.collection('users').find({ email });
+      if (await existingUser > 0) {
         return res.status(400).json({ error: 'Already exist' });
       }
 
-      const hashedPassword = sha1(password);
+      const hashedPassword = createHash('sha1').update(password).digest('hex');
       const newUser = { email, password: hashedPassword };
       const result = await dbClient.db.collection('users').insertOne(newUser);
 
