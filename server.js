97cc269fdb1678c0fabb7fae1270f8fe621fbc05
Selomon1 <soya.vandy1950@gmail.com>
2024-05-17 21:30:51 +0300

    File Manager

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
-import express from 'express';
-import routes from './routes/index';
+const express = require('express');
+const router = require('./routes/index');
 
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(express.json());
-app.use('/', routes);
+app.use('/', router);
 
 app.listen(port, () => {
   console.log(`Server running onport ${port}`);
 });
 
-export default app;
+module.exports = app;
